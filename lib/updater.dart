164e62859f7950e2ff95ef2a2e23a9e34f50078b
Aaron Lee <aaron@nordbot.app>
2023-09-12 11:17:06 -0500

    Moved appcast xml to github pages

--- lib/updater.dart
+++ lib/updater.dart
@@ -2,8 +2,7 @@ import 'package:auto_updater/auto_updater.dart';
 
 class AutoUpdater {
   initUpdate() async {
-    String feedURL =
-        'https://raw.githubusercontent.com/HankBoone/podswitch/master/appcast/appcast.xml';
+    String feedURL = 'https://hankboone.github.io/podswitch/appcast.xml';
     await autoUpdater.setFeedURL(feedURL);
     await autoUpdater.checkForUpdates(
       inBackground: true,
