4895821535b93eb395d29ed0011e3e52e8921adb
Aaron Lee <aaron@nordbot.app>
2023-09-12 06:29:32 -0500

    Housekeeping, organized functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:podswitch/dll.dart';
+import 'package:podswitch/updater.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'pages/home.dart';
-import 'package:auto_updater/auto_updater.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  DynamicLibraryLoader.loadLibs();
+  AutoUpdater().initUpdate();
   databaseFactory = databaseFactoryFfi;
-  String feedURL = 'https://raw.githubusercontent.com/HankBoone/podswitch/master/appcast/appcast.xml';
-  await autoUpdater.setFeedURL(feedURL);
-  await autoUpdater.checkForUpdates();
-  await autoUpdater.setScheduledCheckInterval(3600);
   runApp(const MyApp());
 }
 
---
+++ lib/updater.dart
@@ -0,0 +1,11 @@
+import 'package:auto_updater/auto_updater.dart';
+
+class AutoUpdater {
+  initUpdate() async {
+    String feedURL =
+        'https://raw.githubusercontent.com/HankBoone/podswitch/master/appcast/appcast.xml';
+    await autoUpdater.setFeedURL(feedURL);
+    await autoUpdater.checkForUpdates();
+    await autoUpdater.setScheduledCheckInterval(3600);
+  }
+}
